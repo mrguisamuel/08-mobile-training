8547558eb606597190b372c5cbc93fbe961a2611
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-14 17:26:58 -0300

    refactor: onboarding class

--- madventure/lib/onboarding.dart
+++ madventure/lib/onboarding.dart
@@ -1,13 +1,72 @@
 import 'package:flutter/material.dart';
 
-class OnBoarding extends StatefulWidget {
+class Onboarding extends StatefulWidget {
   @override
-  State<_OnBoardingState> createState() => _OnBoardingState();
+  State<Onboarding> createState() => _OnboardingState();
 }
 
-class _OnBoardingState extends State<OnBoarding> {
+class OnboardPages {
+  BuildContext context;
+  final Color color;
+  final String urlImage, title, subtitle;
+
+  OnboardPages({
+    required this.context,
+    required this.color,
+    required this.urlImage,
+    required this.title,
+    required this.subtitle
+  }) => build();
+
+  Widget build() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          urlImage,
+          fit: BoxFit.cover,
+          width: MediaQuery.of(this.context).size.width
+        ),
+        Text(
+          title
+        )
+      ]
+    );
+  }
+}
+
+class _OnboardingState extends State<Onboarding> {
+  late PageController _pageController;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return 
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          child: PageView(
+            children: <Widget>[
+              buildPage(
+                context: context,
+                urlImage: 'assets/images/onboarding-1.jpg',
+                color: Colors.green,
+                title: 'Teste1',
+                subtitle: 'opa'
+              )
+            ]
+          )
+        )
+      )
+    );
   }
 }
