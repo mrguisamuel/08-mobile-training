30623947c9a03ab3fab8c11971e4857bd4b74c05
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-16 16:22:33 -0300

    feat: add a custom search bar

--- wsa_events/lib/select_event.dart
+++ wsa_events/lib/select_event.dart
@@ -29,20 +29,91 @@ class EventListView extends StatelessWidget {
   }
 }
 
-class SelectEvent extends StatelessWidget {
+class SelectEvent extends StatefulWidget {
+  @override
+  State<SelectEvent> createState() => _SelectEventState();
+}
+
+class _SelectEventState extends State<SelectEvent> {
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = const Text('Digite um evento');
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const IconButton(
+        title: customSearchBar,
+        leading: customIcon.icon == Icons.search ? const IconButton(
           icon: Icon(
             Icons.menu
           ),
           onPressed: null
-        ),
-        title: Text('Escolha um evento!')
+        ) : null,
+        actions: [
+          IconButton(
+            icon: customIcon,
+            onPressed: () {
+              setState(() {
+                if(customIcon.icon == Icons.search) {
+                  customIcon = const Icon(Icons.cancel);
+                  customSearchBar = const ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 28
+                    ),
+                    title: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Digite um evento',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic
+                        ),
+                      )
+                    )
+                  );
+                } else {
+                  customIcon = const Icon(Icons.search);
+                  customSearchBar = const Text('Digite um evento');
+                }
+              });
+            }
+          )
+        ]
       ),
       body: EventListView()
     );
   }
 }
+
+/*
+class MySearchBar extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: () => close(context, null)
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.clear),
+      onPressed: () {
+        if(query.isEmpty)
+          close(context, null);
+        else
+          query = '';
+      }
+    );
+  }
+
+  
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions 
+  }
+}
+*/
