cf44e8b2cdc9b3150099beec8e356f8239690272
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-17 17:25:40 -0300

    still init screen

--- strooper/lib/main.dart
+++ strooper/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'game.dart';
+import 'init.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Game() 
+      home: Init() 
     );
   }
 }
