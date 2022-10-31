812a6b6150cfb49a72e5e6ffccf5f87883639316
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-31 17:27:30 -0300

    refactor: setup the results screen after game

--- strooper/lib/results.dart
+++ strooper/lib/results.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'session.dart';
+import 'my_widgets.dart';
 
 class Results extends StatelessWidget {
   const Results({super.key});
 
+  double meanReactTime() {
+    return Session.totalReactTime / Session.buttonSelectCounter;
+  }
+
+  double meanCorrectWords() {
+    return Session.correctWords / Session.totalWords;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(30),
           child: Column(
             children: <Widget>[
-              Text('Jogo Terminado!\nResultados:\nPontos:\nTempo Médio:\n'),
+              Text('Jogo Terminado!\n
+                Resultados:\n
+                Pontos: ${Session.points}\n
+                Tempo Médio de reação: ${this.meanReactTime()}\n
+                Média de acertos: ${this.meanCorrectWords()}\n'
+              ),
+              Button(
+                message: 'Compartilhar no Facebook',
+                width: size.width * 0.8;
+              ),
+              Button(
+                message: 'Compartilhar no Twitter',
+                width: size.width * 0.8;
+              ),
+              Button(
+                message: 'Ver a lista de high scores',
+                width: size.width * 0.8;
+              ),
             ]
           )
         )
