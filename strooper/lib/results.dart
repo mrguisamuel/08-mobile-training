700600e6ac4d72d38e1bb1a01042ef62deb7f86a
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-01 15:44:37 -0300

    feat: add a record into local database

--- strooper/lib/results.dart
+++ strooper/lib/results.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'session.dart';
 import 'my_widgets.dart';
 import 'database.dart';
+import 'utility.dart';
 
 class Results extends StatelessWidget {
   const Results({super.key});
@@ -10,14 +11,24 @@ class Results extends StatelessWidget {
     if(Session.buttonSelectCounter == 0)
       return 0;
     else
-      return Session.totalReactTime / Session.buttonSelectCounter;
+      return Utility.roundDouble(Session.totalReactTime / Session.buttonSelectCounter, 2);
   }
 
   double averageCorrectWords() {
     if(Session.buttonSelectCounter == 0)
       return 0;
     else
-      return Session.correctWords / Session.totalWords;
+      return Utility.roundDouble(Session.correctWords / Session.totalWords, 2);
+  }
+
+  Future<void> updateDatabase() async {
+    await MyDatabase.instance.insertRecord(
+      Record(
+        points: Session.points,
+        averageReactTime: this.averageReactTime(),
+        averageCorrectWords: this.averageCorrectWords()
+      )
+    );
   }
 
   @override
@@ -31,7 +42,7 @@ class Results extends StatelessWidget {
             children: <Widget>[
               Text('Jogo Terminado!\n', textAlign: TextAlign.center),
               Text('Pontos: ${Session.points}\n', textAlign: TextAlign.center),
-              Text('Tempo Médio de reação: ${this.averageReactTime()}\n', textAlign: TextAlign.center),
+              Text('Tempo Médio de Reação: ${this.averageReactTime()}\n', textAlign: TextAlign.center),
               Text('Média de acertos: ${this.averageCorrectWords()}\n', textAlign: TextAlign.center),
               Button(
                 message: 'Compartilhar no Facebook',
@@ -42,8 +53,9 @@ class Results extends StatelessWidget {
                 width: size.width * 0.8
               ),
               Button(
-                message: 'Ver a lista de high scores',
-                width: size.width * 0.8
+                message: 'Salvar pontuação',
+                width: size.width * 0.8,
+                action: () async => updateDatabase()
               ),
             ]
           )
