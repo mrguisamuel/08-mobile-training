a971c953759f64c8e758266e9d10ee9266eda29a
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-04 17:27:24 -0300

    refactor(tab): organize the type of record using tabs

--- strooper/lib/high_score.dart
+++ strooper/lib/high_score.dart
@@ -9,58 +9,74 @@ class HighScore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_rounded),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Init()
-              )
-            );
-          },
+    return DefaultTabController( 
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_rounded),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Init()
+                )
+              );
+            },
+          ),
+          title: const Text('High Scores!'),
+          bottom: const TabBar(
+            indicatorColor: Colors.black,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home, color: Colors.grey),
+                child: Text('Jogos Normais')
+              ),
+              Tab(
+                icon: Icon(Icons.home, color: Colors.grey),
+                child: Text('Jogos Personalizados')
+              ),
+            ]
+          )
         ),
-        title: const Text('High Scores!')
-      ),
-      body: SafeArea(
-        child: Center(
-          child: FutureBuilder<List<Record>>(
-            future: MyDatabase.instance.getAllRecords(),
-            builder: (BuildContext context, AsyncSnapshot<List<Record>> snapshot) {
-              if(!snapshot.hasData)
-                return CircularProgressIndicator();
-              return snapshot.data!.isEmpty ?
-              Center(child: Text('Sem recordes salvos ainda.')) :
-              ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  String gameState = snapshot.data![index].isDefaultGame == 0 ? 'Jogo Normal' : 'Jogo Personalizado' ;
-                  return Center(
-                    child: ListTile(
-                      title: Text('${index + 1}º - ' + 
-                        '${snapshot.data![index].points} pontos' +
-                        '\nMédia de palavras acertadas: ' + 
-                        '${Utility.roundDouble(snapshot.data![index].averageCorrectWords * 100, 2)}%\n' +
-                        gameState + '\n' 
-                      ),
-                    )
-                  );
-                }
-              );
-              /*
-              ListView(
-                children: snapshot.data!.map((record) {
-                  return Center(
-                    child: ListTile(
-                      title: Text('${record.points} pontos'),
-                    )
-                  );
-                }).toList(),
-              );
-              */
-            }
+        body: SafeArea(
+          child: Center(
+            child: FutureBuilder<List<Record>>(
+              future: MyDatabase.instance.getAllRecords(),
+              builder: (BuildContext context, AsyncSnapshot<List<Record>> snapshot) {
+                if(!snapshot.hasData)
+                  return CircularProgressIndicator();
+                return snapshot.data!.isEmpty ?
+                Center(child: Text('Sem recordes salvos ainda.')) :
+                ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String gameState = snapshot.data![index].isDefaultGame == 0 ? 'Jogo Normal' : 'Jogo Personalizado' ;
+                    return Center(
+                      child: ListTile(
+                        title: Text('${index + 1}º - ' + 
+                          '${snapshot.data![index].points} pontos' +
+                          '\nMédia de palavras acertadas: ' + 
+                          '${Utility.roundDouble(snapshot.data![index].averageCorrectWords * 100, 2)}%\n' +
+                          gameState + '\n' 
+                        ),
+                      )
+                    );
+                  }
+                );
+                /*
+                ListView(
+                  children: snapshot.data!.map((record) {
+                    return Center(
+                      child: ListTile(
+                        title: Text('${record.points} pontos'),
+                      )
+                    );
+                  }).toList(),
+                );
+                */
+              }
+            )
           )
         )
       )
