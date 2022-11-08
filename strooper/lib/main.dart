0ed9b22319470a7b954ccb5c254074b2180af965
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-08 16:07:39 -0300

    refactor(theme): add the dark and light themes arguments

--- strooper/lib/main.dart
+++ strooper/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'init.dart';
+import 'themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme,
       home: Init() 
     );
   }
