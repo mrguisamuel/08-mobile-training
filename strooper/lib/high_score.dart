71be173c43826fc6ee837ffb4115e0b5eb25dce9
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-08 14:36:05 -0300

    refactor(screen): finish screen placing a tabview using the specific record function

--- strooper/lib/high_score.dart
+++ strooper/lib/high_score.dart
@@ -6,6 +6,43 @@ import 'utility.dart';
 class HighScore extends StatelessWidget {
   const HighScore({Key? key}) : super(key: key);
 
+  Widget _getSpecificScores(bool isDefaultGame) {
+    return FutureBuilder<List<Record>>(
+      future: MyDatabase.instance.getAllRecordsByType(isDefaultGame),
+      builder: (BuildContext context, AsyncSnapshot<List<Record>> snapshot) {
+        if(!snapshot.hasData)
+          return CircularProgressIndicator();
+        return snapshot.data!.isEmpty ?
+        Center(child: Text('Sem recordes salvos ainda.')) :
+        ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              child: ListTile(
+                title: Text('${index + 1}º - ' + 
+                  '${snapshot.data![index].points} pontos' +
+                  '\nMédia de palavras acertadas: ' + 
+                  '${Utility.roundDouble(snapshot.data![index].averageCorrectWords * 100, 2)}%\n'
+                ),
+              )
+            );
+          }
+        );
+        /*
+        ListView(
+          children: snapshot.data!.map((record) {
+            return Center(
+              child: ListTile(
+                title: Text('${record.points} pontos'),
+              )
+            );
+          }).toList(),
+        );
+        */
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -29,11 +66,11 @@ class HighScore extends StatelessWidget {
             indicatorColor: Colors.black,
             tabs: [
               Tab(
-                icon: Icon(Icons.home, color: Colors.grey),
+                icon: Icon(Icons.games_rounded, color: Colors.black),
                 child: Text('Jogos Normais')
               ),
               Tab(
-                icon: Icon(Icons.home, color: Colors.grey),
+                icon: Icon(Icons.videogame_asset_rounded, color: Colors.black),
                 child: Text('Jogos Personalizados')
               ),
             ]
@@ -41,41 +78,17 @@ class HighScore extends StatelessWidget {
         ),
         body: SafeArea(
           child: Center(
-            child: FutureBuilder<List<Record>>(
-              future: MyDatabase.instance.getAllRecords(),
-              builder: (BuildContext context, AsyncSnapshot<List<Record>> snapshot) {
-                if(!snapshot.hasData)
-                  return CircularProgressIndicator();
-                return snapshot.data!.isEmpty ?
-                Center(child: Text('Sem recordes salvos ainda.')) :
-                ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    String gameState = snapshot.data![index].isDefaultGame == 0 ? 'Jogo Normal' : 'Jogo Personalizado' ;
-                    return Center(
-                      child: ListTile(
-                        title: Text('${index + 1}º - ' + 
-                          '${snapshot.data![index].points} pontos' +
-                          '\nMédia de palavras acertadas: ' + 
-                          '${Utility.roundDouble(snapshot.data![index].averageCorrectWords * 100, 2)}%\n' +
-                          gameState + '\n' 
-                        ),
-                      )
-                    );
-                  }
-                );
+            child: TabBarView(
+              children: <Widget>[
                 /*
-                ListView(
-                  children: snapshot.data!.map((record) {
-                    return Center(
-                      child: ListTile(
-                        title: Text('${record.points} pontos'),
-                      )
-                    );
-                  }).toList(),
-                );
-                */
-              }
+               */
+                SizedBox(
+                  child: _getSpecificScores(true)
+                ),
+                SizedBox(
+                  child: _getSpecificScores(false)
+                )
+              ]
             )
           )
         )
