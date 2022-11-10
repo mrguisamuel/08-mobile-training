1715c85b33bbc914929dec210c0788e8d7acf654
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-10 16:38:51 -0300

    feat(camera): implement camera in tabbarview

--- remind_me/lib/main.dart
+++ remind_me/lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  runApp(
+    MaterialApp(
       initialRoute: '/home',
       routes: {
-        '/home': (context) => const Home()
+        '/home': (context) => Home(camera: firstCamera)
       }
-    );
-  }
+    )
+  );
 }
--- remind_me/lib/screens/home.dart
+++ remind_me/lib/screens/home.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:camera/camera.dart';
+import 'dart:async';
+import 'dart:io';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  final CameraDescription camera;
+
+  const Home({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  Image? lastImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium
+    );
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -24,13 +57,64 @@ class Home extends StatelessWidget {
           )
         ),
         body: SafeArea(
-          child: const TabBarView(
+          child: TabBarView(
             children: <Widget>[
               SizedBox(
-                child: Text('oi')
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: size.height * 0.5,
+                      width: size.width * 0.8,
+                      child: FutureBuilder<void>(
+                        future: _initializeControllerFuture,
+                        builder: (context, snapshot) {
+                          if(snapshot.connectionState == ConnectionState.done) {
+                            return CameraPreview(_controller);
+                          } else {
+                            return const Center(child: CircularProgressIndicator());
+                          }
+                        }
+                      ),
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FloatingActionButton(
+                          child: Text(
+                            'Frente',
+                            textAlign: TextAlign.center
+                          ),
+                          onPressed: null
+                       ),
+                        SizedBox(width: 30),
+                        FloatingActionButton(
+                          child: Text(
+                            'Trás',
+                            textAlign: TextAlign.center
+                          ),
+                          onPressed: null
+                        )
+                      ]
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                      child: Text('Tirar foto'),
+                      onPressed: () async {
+                        try {
+                          await _initializeControllerFuture;
+                          final image = await _controller.takePicture();
+                          setState(() => this.lastImage = Image.file(File(image.path)));
+                        } catch(e) {
+                          print(e);
+                        }
+                      }
+                    )
+                  ]
+                )
               ),
               SizedBox(
-                child: Text('fdsjhfjdshjfkh')
+                child: this.lastImage == null ? Text('tire a foto!') : this.lastImage
               )
             ]
           )
