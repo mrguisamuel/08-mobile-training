00a68eb133d2a7a946ffff1ac459bff6bb8f502a
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-06 17:19:45 -0300

    style: rename response variables and return type

--- wsa_events/lib/services.dart
+++ wsa_events/lib/services.dart
@@ -3,13 +3,13 @@ import 'dart:core';
 import 'models.dart';
 
 class EventService {
-  Future<List<Events>> getEvents() async {
+  Future<List<Event>> getEvents() async {
     var values = await http.get(Uri.parse('https://spskills-events.azurewebsites.net/eventos'));
 
     // 200 = HTTP GET status code OK
     if(values.statusCode == 200) {
-      var result = response.body as Map<String, dynamic>;
-      return Events.fromJson(result);
+      var result = values.body;
+      return eventsFromJson(result);
     } else throw Exception('Failed to load events');
   }
 }
