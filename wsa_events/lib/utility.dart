47621e6aeb44544227e9d51c0de1707632c22b19
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-13 15:44:18 -0300

    feat: add capitalize functions for string objects

--- wsa_events/lib/utility.dart
+++ wsa_events/lib/utility.dart
@@ -9,3 +9,18 @@ List<String> pickHourAndDate(String dateHour) {
   final hour = dt[1].substring(0, 5);
   return [hour, date];
 }
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
+
+  List<String> splitAndCapitalize(String char) {
+    List<String> ss = this.split(char);
+    List<String> a = [];
+    for(String s in ss){
+      a.add(s.capitalize());
+    }
+    return a;
+  }
+}
