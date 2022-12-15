43ae37fd04bc8efc0ed9890adca5607eac4d22cd
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-15 17:03:28 -0300

    feat: create the notification options

--- wsa_events/lib/settings.dart
+++ wsa_events/lib/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 enum NotificationType {
+  none,
   all,
   specific
 }
@@ -13,7 +14,8 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  NotificationType _notify = NotificationType.all;
+  NotificationType _notify = NotificationType.none;
+  List<bool> _notifyTypes = [false, false, false];
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +38,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 )
               ),
               RadioListTile<NotificationType>(
+                title: Text('Nenhum evento'),
+                value: NotificationType.none,
+                groupValue: this._notify,
+                onChanged: (NotificationType? value) {
+                  setState(() => this._notify = value!);
+                }
+              ),
+              RadioListTile<NotificationType>(
                 title: Text('Todos os eventos'),
                 value: NotificationType.all,
                 groupValue: this._notify,
@@ -51,6 +61,37 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   setState(() => this._notify = value!);
                 }
               ),
+              Visibility(
+                child: Column(
+                  children: <Widget>[
+                    CheckboxListTile(
+                      title: const Text('Título'),
+                      value: this._notifyTypes[0],
+                      secondary: const Icon(Icons.account_tree_rounded),
+                      onChanged: (bool? value) {
+                        setState(() => this._notifyTypes[0] = value!);
+                      }
+                    ),
+                    CheckboxListTile(
+                      title: const Text('Descrição'),
+                      value: this._notifyTypes[1],
+                      secondary: const Icon(Icons.account_tree_rounded),
+                      onChanged: (bool? value) {
+                        setState(() => this._notifyTypes[1] = value!);
+                      }
+                    ),
+                    CheckboxListTile(
+                      title: const Text('Participantes'),
+                      value: this._notifyTypes[2],
+                      secondary: const Icon(Icons.account_tree_rounded),
+                      onChanged: (bool? value) {
+                        setState(() => this._notifyTypes[2] = value!);
+                      }
+                    ),
+                  ]
+                ),
+                visible: this._notify == NotificationType.specific
+              )
             ]
           )
         )
