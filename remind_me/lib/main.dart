4b9d21707878676438ce7313f40d525a001cd1f3
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-16 17:26:44 -0300

    refactor: setup to new screen (using image_picker_

--- remind_me/lib/main.dart
+++ remind_me/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'screens/welcome.dart';
+import 'screens/pickedphoto.dart';
+/*
 import 'screens/home.dart';
 import 'package:camera/camera.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +15,19 @@ Future<void> main() async {
       initialRoute: '/home',
       routes: {
         '/home': (context) => Home(cameras: cameras),
+      }
+    )
+  );
+}
+*/
+
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/welcome',
+      routes: {
+        '/welcome': (BuildContext context) => const Welcome(),
+        '/pickedphoto': (BuildContext context) => const PickedPhoto(),
       }
     )
   );
