63a91a5b965514333c2ba5e252327e19c7c314e7
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-02 15:21:24 -0300

    feat: add watermark logo in image

--- remind_me/lib/screens/editor.dart
+++ remind_me/lib/screens/editor.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import '../utils/globals.dart';
+import 'package:image/image.dart' as ui;
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
-class Editor extends StatelessWidget {
-  final Image baseImage;
+class Editor extends StatefulWidget {
+  const Editor({Key? key}) : super(key: key);  
 
-  const Editor({Key? key, required this.baseImage}) : super(key: key);  
-  
+  @override
+  State<Editor> createState() => _EditorState();
+}
+
+class _EditorState extends State<Editor> {
+  Uint8List? _imgBytes;
+
+  @override
+  void initState() {
+    super.initState();
+    this._addImageWatermark();
+  }
+
+  Future<void> _addImageWatermark() async {
+    final w = await rootBundle.load('assets/images/world-skills-logo.png');
+    final wt = w.buffer.asUint8List();
+
+    final originalImage = ui.decodeImage(await Globals.pickedImage!.readAsBytes());
+    final watermark = ui.decodeImage(wt);
+
+    final image = ui.Image(watermark!.width, watermark!.height);
+    ui.drawImage(image, watermark!);
+
+    int x = (originalImage!.width / 2).round();
+    int y = (originalImage!.height / 2).round();
+
+    ui.copyInto(
+      originalImage!,
+      image,
+      dstX: x,
+      dstY: y,
+    );
+
+    final wmImage = ui.encodePng(originalImage);
+    final result = Uint8List.fromList(wmImage);
+
+    setState(() => this._imgBytes = result);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +56,8 @@ class Editor extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Container(
-                child: this.baseImage
+                child: this._imgBytes == null ? 
+                CircularProgressIndicator() : Image.memory(this._imgBytes!)
               )
             ]
           )
