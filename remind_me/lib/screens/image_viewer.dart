6261d4841d7348fee088c24d0ccebf76d392cbb9
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-06 15:17:56 -0300

    feat: show all images in a grid view

--- remind_me/lib/screens/image_viewer.dart
+++ remind_me/lib/screens/image_viewer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/wsa_image.dart';
+import '../providers/database.dart';
 
 class ImageViewer extends StatefulWidget {
   const ImageViewer({Key? key}) : super(key: key);
@@ -11,14 +13,23 @@ class _ImageViewerState extends State<ImageViewer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Galeria')),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              
-            ]
+          child: FutureBuilder<List<WSAImage>>(
+            future: MyDatabase.getAllImages(),
+            builder: (BuildContext context, AsyncSnapshot<List<WSAImage>> snapshot) {
+              if(!snapshot.hasData)
+                return CircularProgressIndicator();
+              return snapshot.data!.isEmpty ? Center(child: Text('Sem fotos disponíveis...')) :
+              GridView.count(
+                crossAxisCount: 2,
+                children: snapshot.data!.map((wsaImage) {
+                  return Container(child: Image.memory(wsaImage.image));
+                }).toList()
+              );
+            }
           )
         )
       )
