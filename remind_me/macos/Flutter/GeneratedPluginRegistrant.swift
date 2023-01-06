4de8eb81a300b86e12a1cd5527f3ed5c652378dc
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-06 16:07:40 -0300

    chore: add the audioplayers dependence package

--- remind_me/macos/Flutter/GeneratedPluginRegistrant.swift
+++ remind_me/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import geolocator_apple
 import location
 import path_provider_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
