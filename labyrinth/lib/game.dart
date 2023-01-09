d4d867ed398364e3267ba829395876c442a0d4d7
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-09 15:27:11 -0300

    feat: create base player

--- labyrinth/lib/game.dart
+++ labyrinth/lib/game.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart' hide Colors;
+import 'package:motion_sensors/motion_sensors.dart';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -8,11 +10,38 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  Vector3 _gyroscope = Vector3.zero();
+
+  @override
+  void initState() {
+    super.initState();
+    motionSensors.gyroscope.listen((GyroscopeEvent event) {
+      setState(() => this._gyroscope.setValues(event.x, event.y, event.z));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(this._gyroscope.x.toString()),
+      ),
       body: SafeArea(
-        child: Text('eae')
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 50,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(40)
+                )
+              )
+            )
+          ]
+        )
       )
     );
   }
