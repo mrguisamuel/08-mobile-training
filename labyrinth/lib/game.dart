5c1a8fe39e0df7f7f459ee095c165b1b81da40a2
Guilherme Samuel <guisamuel800@gmail.com>
2023-01-09 14:47:32 -0300

    feat: create game base screen

---
+++ labyrinth/lib/game.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  const Game({Key? key}) : super(key: key);
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('eae')
+      )
+    );
+  }
+}
